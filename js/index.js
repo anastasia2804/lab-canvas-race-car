123a0b121310f3d39b6ed6128e0c051fcb5d9774
Anastasia Orlova <anastasia2804@gmail.com>
2022-08-27 07:33:21 -0400

    canvas set up, car is moving on keydown

--- js/index.js
+++ js/index.js
@@ -2,6 +2,42 @@ window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame();
   };
+}
 
-  function startGame() {}
+let car = {
+  x: 225,
+  y: 300,
+}
+
+  function startGame() {
+    const canvas = document.querySelector('#canvas');
+    canvas.style.display = "block"
+    const ctx = canvas.getContext('2d');
+    canvas.style.border = "1px solid black";
+    const carImg = new Image();
+    carImg.src = '../images/car.png'; 
+    carImg.addEventListener('load', () => {
+      ctx.drawImage(carImg, car.x, car.y, 50,100)
+    });
+
+    setInterval (() =>{
+     ctx.clearRect(0,0, canvas.width, canvas.height);
+     ctx.drawImage(carImg, car.x, car.y, 50,100)
+    }, 20)
 };
+
+
+window.addEventListener('keydown', function(event){
+    
+    if(event.code == 'ArrowRight'){
+      car.x += 10
+    }
+    
+    if(event.code == 'ArrowLeft'){
+      car.x -= 10
+    }
+    
+  })
+
+
+  
