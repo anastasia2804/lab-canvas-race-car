b0228264b228046fa68debaf552657875b12d80b
Anastasia Orlova <anastasia2804@gmail.com>
2022-09-02 15:07:14 -0400

    obstacles are moving

--- js/index.js
+++ js/index.js
@@ -4,15 +4,18 @@ window.onload = () => {
   };
 }
 
+const myObstacles = []
+const canvas = document.querySelector('#canvas');
+const ctx = canvas.getContext('2d');
+let myFrames = 0;
+
 let car = {
   x: 225,
   y: 300,
 }
 
   function startGame() {
-    const canvas = document.querySelector('#canvas');
     canvas.style.display = "block"
-    const ctx = canvas.getContext('2d');
     canvas.style.border = "1px solid black";
     const carImg = new Image();
     carImg.src = '../images/car.png'; 
@@ -21,23 +24,59 @@ let car = {
     });
 
     setInterval (() =>{
+      myFrames ++
+
+      if (myFrames % 120 === 0) {
+    let minWidth = 30;
+    let range = 100;
+    let width = Math.floor(Math.random()* range + minWidth) 
+    let xPosition = Math.floor(Math.random()*canvas.width)
+    myObstacles.push(new Component(width, 20, 'black', xPosition , 0));
+      }
+    
+
      ctx.clearRect(0,0, canvas.width, canvas.height);
      ctx.drawImage(carImg, car.x, car.y, 50,100)
+      updateObstacles()
     }, 20)
 };
 
 
 window.addEventListener('keydown', function(event){
-    
     if(event.code == 'ArrowRight'){
+      if (car.x < 380) {
       car.x += 10
+    }
     }
     
     if(event.code == 'ArrowLeft'){
-      car.x -= 10
+      if (car.x > 70){
+      car.x -= 10 
     }
-    
+    }
   })
 
-
   
+
+  class Component {
+    constructor (width, height, color, x, y){
+      this.width = width;
+      this.height = height;
+      this.color = color;
+      this.x = x;
+      this.y = y;
+    }
+
+    draw () {
+      ctx.fillStyle = this.color;
+      ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+  }
+ 
+  function updateObstacles () {
+    for (let i = 0; i < myObstacles.length; i++){
+      myObstacles[i].y += 5;
+      myObstacles[i].draw()
+    }
+    
+  }
